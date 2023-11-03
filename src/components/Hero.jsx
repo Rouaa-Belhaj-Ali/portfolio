9652c8d67411d79fd91fa6b26434f71fdd1cf605
rouaa belhaj ali <hzoe598@gmail.com>
2023-11-03 02:00:48 +0100

    init vite project

---
+++ src/components/Hero.jsx
@@ -0,0 +1,67 @@
+
+
+
+
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+
+
+
+const Hero = () => {
+  return ( 
+  <section className={`relative w-full h-screen mx-auto`}>
+  <div
+    className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+  >
+    <div className='flex flex-col justify-center items-center mt-5'>
+      <div className='w-5 h-5 rounded-full bg-[#ED2647]' />
+      <div className='w-1 sm:h-80 h-40 violet-gradient' />
+    </div>
+
+    <div>
+
+   
+    <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, Im <span className='text-[#ED2647]'>Rouaa</span>
+          </h1>
+      <div className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I am a Software Developer 
+            <p>The IT Girl </p>
+          </div>
+    </div>
+
+    </div>
+
+    <ComputersCanvas />
+
+
+    <div className='absolute xs:bottom-10
+     bottom-32 w-full flex justify-center
+      items-center'>
+        <a href='#about'>
+          <div className='w-[30px] h-[35px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 9, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'/>
+
+            
+
+      </div>
+      </a>
+      </div>
+    </section>
+  
+
+  );
+};
+
+export default Hero;
---
+++ src/constants/index.js
@@ -0,0 +1,269 @@
+import {
+    
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    
+    jobit,
+    tripguide,
+    threejs,
+    freelance,
+    heptacom,
+    saas,
+    si
+ 
+  
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "Flutter Developer",
+     
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Content Creator",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "React.js Developer",
+      company_name: "Freelance",
+      icon: freelance,
+      iconBg: "#383E56",
+      date: "March 2020 - April 2021",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "React Native Developer",
+      company_name: "Freelance",
+      icon: freelance,
+      iconBg: "#E6DEDD",
+      date: "Jan 2021 - Feb 2022",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Web Developer",
+      company_name: "S2I",
+      icon: si,
+      iconBg: "#383E56",
+      date: "Jan 2020- Jan 2021",
+      points: [
+        "Developing and maintaining web applications using c# and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Full stack Developer",
+      company_name: "HeptaCom",
+      icon: heptacom,
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - Present",
+      points: [
+        "Developing and maintaining web applications using SpringBoot and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rouaa proved me wrong.",
+      name: "Alia ",
+      designation: "CFO",
+      company: "HeptaCom",
+      image: "",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rouaa does.",
+      name: "Mohamed Bouafif",
+      designation: "CEO",
+      company: "S2I",
+      image:"https://www.flaticon.com/free-icon/man_2202112?term=avatar&page=1&position=1&origin=tag&related_id=2202112",
+    },
+    {
+      testimonial:
+        "Rouaa's relentless dedication and ability to turn setbacks into opportunities consistently add remarkable value and energy to our projects.",
+      name: "Amine Sghaier",
+      designation: "Program manager",
+      company: "Novation City",
+      image: "",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Newton IA",
+      description:
+        "Newton is not just an app; it's a leap into the future of SaaS platforms. By combining cutting-edge technologies and thoughtful design, it redefines what users can expect from a modern, AI-powered solution. Experience the future of user interactions with Newtonn.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: saas,
+      source_code_link: "https://github.com/Rouaa-Belhaj-Ali/NewtonAI",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects   };
